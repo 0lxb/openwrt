5f7580a25623fb8bbe4cec289c5bee7f4e3be5de
Xiaobing Luo <luoxiaobing0926@gmail.com>
2023-06-01 18:21:32 +0000

    wdr6500

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -681,12 +681,13 @@ define Device/tplink_tl-wdr6500-v2
   SOC := qca9561
   DEVICE_MODEL := TL-WDR6500
   DEVICE_VARIANT := v2
-  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca988x-ct
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-ledtrig-usbdev \
+	kmod-ath9k kmod-ath10k-ct ath10k-firmware-qca988x-ct
   IMAGE_SIZE := 8000k
   KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
-  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | uImage lzma | tplink-v1-header
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | uImage lzma
   TPLINK_HWID := 0x65000002
-  TPLINK_HEADER_VERSION := 2
+  TPLINK_HEADER_VERSION := 1
   SUPPORTED_DEVICES += tl-wdr6500-v2
 endef
 TARGET_DEVICES += tplink_tl-wdr6500-v2
